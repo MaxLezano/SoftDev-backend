7237d60386f47ae4317aaa78666f6689b733544b
marcosr87 <marcosr_87@hotmail.com>
2022-08-06 11:40:10 -0300

    Correcciones de sintaxis

--- models/productsModel.js
+++ models/productsModel.js
@@ -6,11 +6,11 @@ const productSchema = new Schema({
     type: Number,
     required: [true, `El precio es requerido`], 
   },
-  category : {type: String, required: true , unique : true ,trim: true},
+  category : {type: String, required: true ,trim: true},
   description : {type : String, trim : true},
   img: { type: String, required : true, default: "https://img.freepik.com/vector-premium/icono-marco-fotos-foto-vacia-blanco-vector-sobre-fondo-transparente-aislado-eps-10_399089-1290.jpg" },
   stock: { type: Number, required: true },
-  IsAprobed: { type: Boolean, default: false },
+  IsAproved: { type: Boolean, default: false },
 });
 
 module.exports = model("product", productSchema);
